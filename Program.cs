8bbd2245d68a65e135d800bf41c2596a93db8016
DESKTOP-UJO5UUV\DELL <yachita.poohping@mail.kmutt.ac.th>
2021-03-28 12:20:23 +0700

    test2ja

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace testsat
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ข้อ2เรียบร้อย
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int x = a;
+            int y = b;
+
+            while (x < y || x > y)
+            {
+                if (x < y)
+                {
+                    x = x + a;
+                }
+                else 
+                {
+                   y = y + b;
+                }
+            }
+            if (x == y)
+            {
+                Console.WriteLine(x);
+            }
+            else 
+            {
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+
+        }
+    }
+}
